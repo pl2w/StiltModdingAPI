15f3e4a56d1a47f079eb0bf8ccc6669ccdea9f96
MrBanana <72209985+MrBanana01@users.noreply.github.com>
2024-03-14 11:32:39 -0500

    Add files via upload

---
+++ StiltModdingAPI/Behaviours/Events/StiltEventHandler.cs
@@ -0,0 +1,24 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace StiltModdingAPI.Behaviours.Events {
+    public class StiltEventHandler : MonoBehaviour {
+        public delegate void SceneLoaded(bool ContainsPlayerPrefab, bool IsMultiplayerScene);
+        /// <summary>
+        /// Gets called a new scene of stilt loads, use this instead of the normal unity scene loaded event.
+        /// </summary>
+        public static event SceneLoaded OnSceneLoaded;
+
+        public static void CallEvent(int EventID, object[] Data) {
+            MelonLogger.Msg($"event called, {EventID}, {Data[0]}, {Data[1]}");
+            if (Data == null) return;
+
+            switch (EventID) {
+                case 0:
+                    if(OnSceneLoaded != null)
+                        OnSceneLoaded.Invoke((bool)Data[0], (bool)Data[1]);
+                    break;
+            }
+        }
+    }
+}
---
+++ StiltModdingAPI/ModLoader.cs
@@ -0,0 +1,43 @@
+﻿using MelonLoader;
+using StiltModdingAPI;
+using StiltModdingAPI.Behaviours.Events;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(ModLoader), "StiltModdingAPI", "1.0.0", "pl2w & MrBanana")]
+[assembly: MelonGame("Rekt Games", "Stilt")]
+namespace StiltModdingAPI {
+    public class ModLoader : MelonMod {
+        public static ModLoader instance;
+        public GameObject APIManager;
+
+        public override void OnLateInitializeMelon() {
+            base.OnLateInitializeMelon();
+
+            instance = this;
+            CreateManagerForObject();
+        }
+
+        void CreateManagerForObject() {
+            APIManager = new GameObject("StiltModdingAPIManager");
+            APIManager.AddComponent<StiltEventHandler>();
+            APIManager.AddComponent<Inputs>();
+            GameObject.DontDestroyOnLoad(APIManager);
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName) {
+            base.OnSceneWasLoaded(buildIndex, sceneName);
+            GameObject[] allObjs = Resources.FindObjectsOfTypeAll<GameObject>();
+            bool first = false;
+            bool second = false;
+            foreach (GameObject obj in allObjs) {
+                if (obj.name == "PlayerPrefab" || obj.name == "PlayerPrefab(clone)")
+                    first = true;
+
+                if (obj.name == "NetworkTime")
+                    second = true;
+            }
+
+            StiltEventHandler.CallEvent(1, new object[] { first, second });
+        }
+    }
+}
