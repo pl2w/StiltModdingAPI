d24fdf1c07f294503c6cd00086f33e925678a7cd
MrBanana <72209985+MrBanana01@users.noreply.github.com>
2024-03-19 08:49:20 -0500

    Update StiltEventHandler.cs

--- StiltModdingAPI/Behaviours/Events/StiltEventHandler.cs
+++ StiltModdingAPI/Behaviours/Events/StiltEventHandler.cs
@@ -1,25 +1,20 @@
-﻿using MelonLoader;
+using MelonLoader;
 using UnityEngine;
 
-namespace StiltModdingAPI.Behaviours.Events
-{
-    public class StiltEventHandler : MonoBehaviour
-    {
+namespace StiltModdingAPI.Behaviours.Events {
+    public class StiltEventHandler : MonoBehaviour {
         public delegate void SceneLoaded(bool ContainsPlayerPrefab, bool IsMultiplayerScene);
         /// <summary>
         /// Gets called a new scene of stilt loads, use this instead of the normal unity scene loaded event.
         /// </summary>
         public static event SceneLoaded OnSceneLoaded;
 
-        public static void CallEvent(int EventID, object[] Data)
-        {
-            MelonLogger.Msg($"event called, {EventID}, {Data[0]}, {Data[1]}");
+        public static void CallEvent(int EventID, object[] Data) {
             if (Data == null) return;
 
-            switch (EventID)
-            {
+            switch (EventID) {
                 case 0:
-                    if (OnSceneLoaded != null)
+                    if(OnSceneLoaded != null)
                         OnSceneLoaded.Invoke((bool)Data[0], (bool)Data[1]);
                     break;
             }
